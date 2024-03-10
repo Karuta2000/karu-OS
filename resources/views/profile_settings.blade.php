0309bcb34614a641b21acff718567fb62e9359a1
Karuta23 <50730505+Karuta2000@users.noreply.github.com>
2024-03-10 18:55:20 +0100

    Update profile_settings.blade.php

--- resources/views/profile_settings.blade.php
+++ resources/views/profile_settings.blade.php
@@ -1,34 +1,40 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile settings') }}
-        </h2>
-    </x-slot>
+    <div class="py-3 container">
 
-    <div class="py-12 container">
-        <div class="">
-            <div class="card p-4 my-3">
-                <div class="w-100">
-                    <livewire:profile.update-profile-information-form />
-                </div>
+        <div class="card my-3 shadow-sm w-100">
+            <div class="card-header">
+                Profile information
             </div>
-
-            <div class="card p-4 my-3">
-                <div class="w-100">
-                    <livewire:profile.update-avatar-form />
-                </div>
+            <div class="card-body">
+                <livewire:profile.update-profile-information-form />
             </div>
+            
+        </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    <livewire:profile.update-password-form />
-                </div>
+        <div class="card my-3 shadow-sm">
+            <div class="card-header">
+                Avatar and banner
             </div>
+            <div class="card-body">
+                <livewire:profile.update-avatar-form />
+            </div>
+        </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    <livewire:profile.delete-user-form />
-                </div>
+        <div class="card my-3 shadow-sm">
+            <div class="card-header">
+                Password
+            </div>
+            <div class="card-body">
+                <livewire:profile.update-password-form />
+            </div>
+        </div>
+
+        <div class="card my-3 shadow-sm">
+            <div class="card-header">
+                Delete user
+            </div>
+            <div class="card-body">
+                <livewire:profile.delete-user-form />
             </div>
         </div>
     </div>
