31e27f3498f094fa46ad69034f658e269f61b1c3
Karuta23 <50730505+Karuta2000@users.noreply.github.com>
2024-03-22 16:36:46 +0100

    Update view.blade.php

--- resources/views/livewire/projects/project/view.blade.php
+++ resources/views/livewire/projects/project/view.blade.php
@@ -17,16 +17,6 @@
                             @foreach ($project->task_list->tasks as $task)
                                 <li class="list-group-item d-flex justify-content-between align-items-center">
                                     {{ $task->title }}
-                                    <div class="btn-group" role="group" aria-label="Basic example">
-                                        <input type="checkbox" class="btn-check" id="task{{ $task->id }}"
-                                            {{ $task->completed == 1 ? 'checked' : '' }} autocomplete="off"
-                                            wire:click='completeTask({{ $task->id }})'>
-                                        <label class="btn btn-outline-primary" for="task{{ $task->id }}"><i
-                                                class="fa-solid fa-check"></i></label>
-                                        <button type="button" class="btn btn-danger"
-                                            wire:click="deleteTask({{ $task->id }})"><i
-                                                class="fa-solid fa-trash"></i></button>
-                                    </div>
                                 </li>
                             @endforeach
                         @endempty
