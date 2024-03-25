3f7bd4a5ff1c8e7f0630fad666554b3d15e533e0
Karuta23 <50730505+Karuta2000@users.noreply.github.com>
2024-03-25 22:27:17 +0100

    Added modal for removing colors

---
+++ app/Livewire/App/Settings/Modals/ColorModal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\App\Settings\Modals;
+
+use Livewire\Component;
+
+class ColorModal extends Component
+{
+    public function render()
+    {
+        return view('livewire.app.settings.modals.color-modal');
+    }
+}
--- resources/views/livewire/app/settings/colors.blade.php
+++ resources/views/livewire/app/settings/colors.blade.php
@@ -1,24 +1,30 @@
 <div class="card p-5">
-    
+
     <div class="row">
         <div class="col-6">
             <label for="hex" class="form-label">Color: </label>
             <input class="form-control" type="color" name="hex" id="hex" wire:model='hex'>
+
+            <button class="btn btn-primary mt-3" wire:click='addColor()'>
+                Add color
+            </button>
         </div>
-        <button class="btn btn-primary" wire:click='addColor()'>
-            Add color
-        </button>
+
     </div>
 
-    <div class="row mt-2">
+    <div class="row mt-5">
         @foreach ($colors as $color)
-        <div class="col-2">
-            <div class="w-100 p-5 text-center round shadow" style="background-color: {{ $color->hex }}">{{ $color->hex }}</div>
-        </div>
+            <div class="col-2">
+
+                <div wire:click="$dispatch('showModal', {data: {'alias' : 'app.settings.modals.color-modal' }})" class="clickable-card w-100 p-5 text-center round shadow" style="background-color: {{ $color->hex }}">
+                    {{ $color->hex }}</div>
+
+
+            </div>
         @endforeach
-        
+
     </div>
-    
+
 
 
 </div>
