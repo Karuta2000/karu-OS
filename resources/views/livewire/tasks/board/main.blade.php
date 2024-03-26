52552c8de985cf88f81836d2d29ac5a6a020812e
Karuta23 <50730505+Karuta2000@users.noreply.github.com>
2024-03-26 22:30:32 +0100

    change in design in board

--- resources/views/livewire/tasks/board/main.blade.php
+++ resources/views/livewire/tasks/board/main.blade.php
@@ -17,26 +17,28 @@ new class extends Component {
 
     public function updateBoard($id){
         if($id == $this->board->id){
+
             $this->tasks = $this->board->tasks;
         }
     }
+
 };
 
 ?>
 
-<div class="card shadow bg-gradient" style="background-color: {{ $board->color->hex }}77">
-    <div class="card-header p-3">
+<div class="card shadow bg-gradient" style="background-color: {{ $board->color->hex }}44">
+    <div class="card-header bg-transparent p-3">
         <p class="m-0 h5">
             {{ $board->title }}
         </p>
     </div>
-    <div class="card-body">
+    <div class="card-body bg-transparent">
         @foreach ($tasks as $key => $task)
-            <livewire:tasks.board.task id="{{ $task->id }}" />
+            <livewire:tasks.board.task id="{{ $task->id }}" wire:key="$task->id" />
         @endforeach
     </div>
     <div class="card-footer p-0">
-        <button class="btn btn-light w-100" wire:click="$dispatch('showModal', {data: {'alias' : 'tasks.modals.add-task-modal','params' :{'board':'{{ $board->id }}'}  }})">
+        <button class="btn bg-transparent w-100" wire:click="$dispatch('showModal', {data: {'alias' : 'tasks.modals.add-task-modal','params' :{'board':'{{ $board->id }}'}  }})">
             <i class="fa-solid fa-plus"></i>
         </button>
     </div>
